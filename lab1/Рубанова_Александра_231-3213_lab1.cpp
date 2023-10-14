74038ff61a3daeaa4106a733c4f511c06f955eb6
Alex Rubanova <grigorijotrepev7@gmail.com>
2023-10-14 11:57:54 +0300

    Lab commit

---
+++ lab1/Рубанова_Александра_231-3213_lab1.cpp
@@ -0,0 +1,53 @@
+﻿// Рубанова_Александра_231-3213_lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+#include <bitset>
+#include <cfloat>
+
+int main()
+
+{
+    double a;
+    double b;
+    double x1;
+    double x2;
+    int num;
+    double middle;
+    double x;
+    //Exercise № 1
+    std::cout << "Rubanova Alexandra Yuryevna 231-3213\n" << std::endl;
+    //Exercise № 2
+    std::cout << "Maxima and minima of various data types\n" << std::endl;
+    std::cout << " max bool: " << bool(0b11111111) << " min bool: " << bool(0b00000000) << " size of bool: " << sizeof(bool) << " byte\n" << std::endl;
+    std::cout << " max int: " << int(0b01111111111111111111111111111111) << " min int: " << int(0b10000000000000000000000000000000) << " size of int: " << sizeof(int) << " byte\n" << std::endl;
+    std::cout << " max unsigned int: " << ((unsigned int)(0b11111111111111111111111111111111)) << " min unsigned int: " << ((unsigned int)(0b00000000000000000000000000000000)) << " size of unsigned int: " << sizeof(unsigned int) << " byte\n" << std::endl;
+    std::cout << " max short: " << short(0b0111111111111111) << " min short: " << short(0b1000000000000000) << " size of short: " << sizeof(short) << "byte\n" << std::endl;
+    std::cout << " max unsigned short: " << ((unsigned short)(0b11111111111111111111111111111111)) << " min unsigned short: " << ((unsigned short)(0b10000000000000000000000000000000)) << " size of short: " << sizeof(unsigned short) << " byte\n" << std::endl;
+    std::cout << " max long: " << long(0b01111111111111111111111111111111) << " min long: " << long(0b10000000000000000000000000000000) << "size of long: " << sizeof(long) << " byte\n" << std::endl;
+    std::cout << " max long long: " << ((long long)(0b0111111111111111111111111111111111111111111111111111111111111111)) << " min long long: " << ((long long)(0b1000000000000000000000000000000000000000000000000000000000000000)) << " size of long long: " << sizeof(long long) << " byte\n" << std::endl;
+    std::cout << " max char: " << char(0b10000000) << " min char: " << char(0b1111111) << " size of char: " << sizeof(char) << " byte\n" << std::endl;
+    std::cout << " max double: " << double(DBL_MAX) << " min double: " << double(DBL_MIN) << " size of double: " << sizeof(double) << " byte\n" << std::endl;
+
+    //Exercise № 3
+    std::cout << " Enter a number ";
+    std::cin >> num;
+    std::cout << " A number in binary form: " << std::bitset<8 * sizeof(num)>(num) << std::endl;
+    std::cout << " A number in hexadecimal form: " << std::hex << num << std::dec << std::endl;
+    std::cout << " A number in bool: " << bool(num) << std::endl;
+    std::cout << " A number in double " << double(num) << std::endl;
+    std::cout << " A number in char: " << char(num) << std::endl;
+    //Exercise № 4
+    std::cout << " Enter the values for the coefficients in the equation a * x = b\n";
+
+    std::cin >> a >> b;
+    x = b / a;
+    std::cout << " Unknown variable x = " << x << "\n";
+    //Exercise № 5
+    std::cout << " Enter the coordinates of the ends of the segment:\n";
+    std::cin >> x1 >> x2;
+    middle = x1 + double(x2 - x1) / 2;
+    std::cout << " The middle of the segment: " << middle << "\n";
+
+    return 0;
+}
+
---
+++ lab2/Rubanova_Alexandra_Yuryevna_231-3213_lab2.cpp
@@ -0,0 +1,88 @@
+﻿// Rubanova_Alexandra_Yuryevna_231-3213_lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+void triangle(int number)
+{
+	for (int i = 0; i <= number; i++) {
+		for (int j = 0; j <= i; j++) {
+			std::cout << j;
+		}
+		std::cout << std::endl;
+	}
+}
+
+
+
+void task1()
+{
+	int triangle_number;
+	std::cout << "Task 1" << std::endl <<
+		"Insert your number:" << std::endl;
+	std::cin >> triangle_number;
+	triangle(triangle_number);
+	std::cout << std::endl;
+}
+
+void task2()
+{
+	int n, y = 1;
+	std::cout << "Task 2" << std::endl <<
+		"Insert your number:" << std::endl;
+	std::cin >> n;
+
+	for (int k = 0; k <= n; k++) {
+		std::cout << y * ((n - k + 1)/k) << "\t";
+		y = y * ((n - k + 1) / k);
+	}
+	std::cout << std::endl << std::endl;
+}
+
+void task3()
+{
+	int counter = 0, summ = 0;
+	char input;
+	std::cout << "Task 3" << std::endl <<
+		"Input numbers, finishing with !" << std::endl;
+	std::cin >> input;
+	while (input != '!') {
+		summ += (input - 48);
+		counter++;
+		std::cin >> input;
+	}
+	std::cout << "Your average number is " << double(summ) / counter << std::endl << std::endl;
+}
+
+int main()
+{
+	int input;
+	while (true) {
+		std::cout << "Insert 1 to check task number 1" << std::endl <<
+			"Insert 2 to check task number 2" << std::endl <<
+			"Insert 3 to check task number 3" << std::endl <<
+			"Insert anything else to shut down" << std::endl;
+		std::cin >> input;
+		switch (input) {
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+		case 3:
+		{
+			task3();
+			break;
+		}
+		default:
+		{
+			return 0;
+		}
+		}
+	}
+}
